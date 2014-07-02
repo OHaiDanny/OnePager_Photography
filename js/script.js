fc10cd82d599106c0d95b08e5d1da34f264f6740
Danny Fang Yan <OHaiDanny>
2014-07-02 14:20:30 -0700

    animated the mobile menu

--- js/script.js
+++ js/script.js
@@ -1,28 +1,41 @@
 $(document).ready(function () {
-    // load justified gallery
-    $("#body").justifiedGallery();
+    if ($(window).width() >= 845) {
+        // load justified gallery
+        $("#body").justifiedGallery();
 
-    $('#reset').click(function () {
-        reset();
-    });
+        $('#reset').click(function () {
+            reset();
+        });
 
-    $("nav a[href^='#']").click(function () {
-        reset();
-        if ($(this).attr('href').replace('#', '') != 'home') {
-            var category = ['land', 'arch', 'urban'];
-            var toRemove = $(this).attr('href').replace('#', '');
-            category.splice($.inArray(toRemove, category), 1);
-            jQuery.each(category, function (i, val) {
-                $('.' + val + '').animate({
-                    opacity: "0.1"
-                }, 500);
-            });
+        $("nav a[href^='#']").click(function () {
+            reset();
+            if ($(this).attr('href').replace('#', '') != 'home') {
+                var category = ['land', 'arch', 'urban'];
+                var toRemove = $(this).attr('href').replace('#', '');
+                category.splice($.inArray(toRemove, category), 1);
+                jQuery.each(category, function (i, val) {
+                    $('.' + val + '').animate({
+                        opacity: "0.1"
+                    }, 500);
+                });
+            }
+        });
+
+        function reset() {
+            $('#body div').animate({
+                opacity: "1"
+            }, 500);
         }
-    });
-
-    function reset() {
-        $('#body div').animate({
-            opacity: "1"
-        }, 500);
+    } else {
+        $("#body").justifiedGallery();
+        $('nav ul').hide();
+        
+        $('#mobileMenu').on('touchstart click', function (e) {
+            if ($('nav ul').is(':visible')) {
+                $('nav ul').slideUp(500);
+            } else {
+                $('nav ul').slideDown(500);
+            }
+        });
     }
 });
